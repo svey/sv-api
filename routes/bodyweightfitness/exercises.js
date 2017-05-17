8496253247c8338e26e510138fab482d95e76a43
svey <marcus.svehlak@gmail.com>
2017-05-16 20:23:49 -0700

    Flattened data.json

--- routes/bodyweightfitness/exercises.js
+++ routes/bodyweightfitness/exercises.js
@@ -2,6 +2,6 @@ const data = require('../../data.json');
 
 module.exports = (req, res) => {
   const category = req.params.category;
-  const exercises = data.routine.filter(exercise => exercise.category === category);
+  const exercises = data.filter(exercise => exercise.category === category);
   res.status(200).json({ exercises });
 };
--- routes/bodyweightfitness/routine.js
+++ routes/bodyweightfitness/routine.js
@@ -1,6 +1,6 @@
 const data = require('../../data.json');
 
 module.exports = (req, res) => {
-  const routine = data.routine;
+  const routine = data;
   res.status(200).json({ routine });
 };
